3e6bcd35eb2dc64b18a50eaf4747ebd4e29d4a97
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-02-20 21:03:55 +0330

    asdf

--- controllers/rcj_soccer_team_blue/utils.py
+++ controllers/rcj_soccer_team_blue/utils.py
@@ -1,3 +1,6 @@
+import math
+dataFormat = "idd?dd"
+
 def get_direction(ball_vector: list) -> int:
     """Get direction to navigate robot to face the ball
 
@@ -11,3 +14,9 @@ def get_direction(ball_vector: list) -> int:
     if -0.13 <= ball_vector[1] <= 0.13:
         return 0
     return -1 if ball_vector[1] < 0 else 1
+def velocity(v):
+    if v > 10: return 10
+    if v <-10: return -10
+    return v
+def getDistance(p1, p2):
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
